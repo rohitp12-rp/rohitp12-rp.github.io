16577dc8afc810eb660e0537e79d5dc4af47fe6b
Rohit <rohit.p120101@gmail.com>
2023-09-28 20:19:53 +0530

    Add recommendations page

---
+++ scripts/rec_script.js
@@ -0,0 +1,40 @@
+
+// gsap animation 
+
+let books = gsap.utils.toArray('.books');
+books.forEach(book => {
+  gsap.to(book, { 
+    x: 800,
+    scrollTrigger: {
+      trigger: book,
+      start: '-50% center',
+      end: '50% center',
+      scrub: true
+    }
+  })
+});
+// let tl = gsap.timeline({
+//     scrollTrigger:{
+//         trigger: '.books',
+//         start: '-50% center',
+//         end: '50% center',
+//         scrub: true,
+//         markers: true
+//     }
+// })
+// tl.to('.books',{
+//     x:800
+// })
+// Smooth scroll 
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
